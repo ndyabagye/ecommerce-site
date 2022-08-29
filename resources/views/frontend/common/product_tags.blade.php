eb624b86eb83560f946a9f285ff631aa5d8be9df
ndyabagye <ndyabagyehenrytusi@gmail.com>
2022-08-29 13:18:22 +0300

    first commit

--- resources/views/frontend/common/product_tags.blade.php
+++ resources/views/frontend/common/product_tags.blade.php
@@ -2,37 +2,39 @@
 
 $tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
 
-$tags_hin = App\Models\Product::groupBy('product_tags_hin')->select('product_tags_hin')->get();
+// $tags_hin = App\Models\Product::groupBy('product_tags_hin')->select('product_tags_hin')->get();
 @endphp
 
 
 
 
-     <div class="sidebar-widget product-tag wow fadeInUp">
-          <h3 class="section-title">Product tags</h3>
-          <div class="sidebar-widget-body outer-top-xs">
+<div class="sidebar-widget product-tag wow fadeInUp">
+  <h3 class="section-title">Product tags</h3>
+  <div class="sidebar-widget-body outer-top-xs">
 
-<div class="tag-list">
+    <div class="tag-list">
 
-<!-- @if(session()->get('language') == 'hindi')
+      {{--
+      <!-- @if(session()->get('language') == 'hindi')
 
 @foreach($tags_hin as $tag)
 <a class="item active" title="Phone" href="{{ url('product/tag/'.$tag->product_tags_hin) }}">
 	{{ str_replace(',',' ',$tag->product_tags_hin)  }}</a>
 @endforeach
 
- @else -->
+ @else --> --}}
 
-@foreach($tags_en as $tag)
-<a class="item active" title="Phone" href="{{ url('product/tag/'.$tag->product_tags_en) }}">
-	{{ str_replace(',',' ',$tag->product_tags_en)  }}</a>
-@endforeach
+      @foreach($tags_en as $tag)
+      <a class="item active" title="Phone" href="{{ url('product/tag/'.$tag->product_tags_en) }}">
+        {{ str_replace(',',' ',$tag->product_tags_en) }}</a>
+      @endforeach
 
-  <!-- @endif -->
+      {{--
+      <!-- @endif --> --}}
 
-	 </div>
-<!-- /.tag-list -->
+    </div>
+    <!-- /.tag-list -->
+  </div>
+  <!-- /.sidebar-widget-body -->
 </div>
-          <!-- /.sidebar-widget-body -->
-        </div>
-        <!-- /.sidebar-widget -->
+<!-- /.sidebar-widget -->
